094b07536a7235cd20f48583b0353d3ae35084b5
Khoa Ngo <ngohakhoa@gmail.com>
2021-07-17 14:33:29 +0700

    Add examinee routes.

---
+++ src/components/examinee/Default.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Default = () => {
+  return <div>Examinee Default</div>;
+};
+
+export default Default;
---
+++ src/components/examinee/Login.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { navigate } from "gatsby";
+import { handleLogin, isLoggedIn, isBrowser } from "../../utils/auth";
+import { EXAMINEE_ROLE } from "../../utils/roles";
+
+const Login = () => {
+  const role = EXAMINEE_ROLE;
+
+  if (isBrowser && isLoggedIn(role)) {
+    navigate(`/${EXAMINEE_ROLE}`);
+    return <></>;
+  }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const isLoginSuccess = handleLogin({ email, password, role });
+    if (isLoginSuccess) {
+      return navigate(`/${EXAMINEE_ROLE}`);
+    }
+    alert("Invalid login credentials");
+  };
+
+  return (
+    <div
+      className="uk-flex uk-flex-center uk-flex-middle"
+      style={{
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        left: 0,
+        backgroundColor: "#D1D1D1",
+      }}
+    >
+      <div className="uk-width-1-3 uk-background-default uk-border-rounded uk-padding">
+        <form className="uk-form" onSubmit={onSubmit}>
+          <fieldset className="uk-fieldset">
+            <legend className="uk-legend">Login</legend>
+            <div className="uk-margin">
+              <input
+                required
+                className="uk-input"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={handleChangeEmail}
+              />
+            </div>
+            <div className="uk-margin">
+              <input
+                required
+                className="uk-input"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChangePassword}
+              />
+            </div>
+            <button className="uk-button">Login</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
